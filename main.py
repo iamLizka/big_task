22fc50354a4f61998fe356feb9c4311a4cdff23b
iamLizka <lizakozyreva38@yandex.ru>
2024-02-08 21:24:41 +0300

    task 2

--- main.py
+++ main.py
@@ -10,29 +10,40 @@ from PyQt5 import QtCore
 class Prog(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
         self.initUI()
 
     def initUI(self):
         self.setGeometry(700, 300, 900, 550)
 
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
+        self.lon = "44.913507"
+        self.lat = "53.224134"
+        self.scale = 0.002
+
         self.image = QLabel(self)
         self.image.move(300, 25)
         self.image.resize(600, 500)
+
+        self.getImage()
+        self.show_map()
+
+    def show_map(self):
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
 
-        self.scale = 0.002
-
-
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=24.952561%2C60.169489&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        api_server = "http://static-maps.yandex.ru/1.x/"
+
+        params = {
+            "ll": ",".join([self.lon, self.lat]),
+            "spn": ",".join([str(self.scale), str(self.scale)]),
+            "l": "map"
+        }
+        response = requests.get(api_server, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
+            print(response.url)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
@@ -40,6 +51,18 @@ class Prog(QWidget):
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_PageDown:
+            self.scale += 0.001 if self.scale <= 50 else 50
+        elif event.key() == QtCore.Qt.Key_PageUp:
+            self.scale -= 0.001 if self.scale != 0.0 else 0.0
+        self.getImage()
+        self.show_map()
 
 
 if __name__ == "__main__":
